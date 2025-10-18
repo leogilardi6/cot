64007a92531fcfa99e1e0ce8d8743379a03ca909
Leandro Gilardi <leogilardi6@gmail.com>
2025-10-18 10:17:50 -0300

    Updates on publish

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cot-website');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cot');
   });
 });
